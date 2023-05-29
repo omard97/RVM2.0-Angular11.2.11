c9a20786dcf3adda5b5d8aaad3006a0d8591ab3f
Dandrea Omar <omar_dandrea97@hotmail.com>
2023-05-29 08:20:36 -0300

    cdn mapbox en index.html y token mapbox en main.ts

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,16 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
+import mapboxgl from 'mapbox-gl'; // or "const mapboxgl = require('mapbox-gl');"
+ 
+mapboxgl.accessToken = 'pk.eyJ1Ijoib21hcmQ5NyIsImEiOiJjbGhpdGE4d3MwMGNtM2dwc3lnZjc0ZTk3In0.OdWECTi0zDbRihpSeWKSOg';
+
+
+if(!navigator.geolocation){
+  alert('El navegador no sosporta la Geolocalizacion')
+  throw new Error('El navegador no sosporta la Geolocalizacion')
+}
+
 if (environment.production) {
   enableProdMode();
 }
