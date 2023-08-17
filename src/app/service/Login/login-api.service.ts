1ad6a21eb37ef82f0be3149b8dca987ff77c3d05
Dandrea Omar <omar_dandrea97@hotmail.com>
2023-08-17 15:29:19 -0300

    backEnd - metodo para validar el nick del usuario

--- src/app/service/Login/login-api.service.ts
+++ src/app/service/Login/login-api.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { idInicioSesionUsuario } from 'src/app/model/InicioSesion';
 import { sesionUsuario } from 'src/app/model/sesion';
+import { nickUsuario } from 'src/app/model/usuario';
 
 @Injectable({
   providedIn: 'root'
@@ -14,21 +15,28 @@ export class LoginApiService {
     headers: new HttpHeaders({ 'Content-Type': 'application/json' })
   };
 
-  constructor(private http:HttpClient) { }
+  constructor(private http: HttpClient) { }
 
 
-    /* Pantalla sesion - validar usuario y luego loguearse */
-    getValidacionUsuario(email:any, pass:any): Observable<any> {
-      
-      return this.http.get<sesionUsuario[]>('https://localhost:44363/sesion?'+"email="+email+"&"+"password="+pass); /* email=example@hotmail.com&password=123'); */
-    }
-    /* traer el id de sesion y utilizarlo para crear el reclamo */
-    getSesionUsuarioLogueado(idUsuario:number):Observable<idInicioSesionUsuario[]>{
-      return this.http.get<idInicioSesionUsuario[]>('https://localhost:44363/V_ultimaSesionDelUsuario/'+idUsuario);
-    }
+  /* Pantalla sesion - validar usuario y luego loguearse */
+  getValidacionUsuario(email: any, pass: any): Observable<any> {
 
-    /* Post inicio sesion - se registra el logueo del usuario */
-  postInicioSesionUsuario(usuarioLogueado: any):Observable<any>{
+    return this.http.get<sesionUsuario[]>('https://localhost:44363/sesion?' + "email=" + email + "&" + "password=" + pass); /* email=example@hotmail.com&password=123'); */
+  }
+
+  getConfirmarNickUsuario(nick:string){
+    return this.http.get<nickUsuario[]>('https://localhost:44363/V_listaUsuariosNick/' + nick);
+  }
+
+
+
+  /* traer el id de sesion y utilizarlo para crear el reclamo */
+  getSesionUsuarioLogueado(idUsuario: number): Observable<idInicioSesionUsuario[]> {
+    return this.http.get<idInicioSesionUsuario[]>('https://localhost:44363/V_ultimaSesionDelUsuario/' + idUsuario);
+  }
+
+  /* Post inicio sesion - se registra el logueo del usuario */
+  postInicioSesionUsuario(usuarioLogueado: any): Observable<any> {
     return this.http.post('https://localhost:44363/sesion', usuarioLogueado, this.httpOptions);
   }
 }
