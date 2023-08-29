0492a79dd190bb59445e81b80977dad4a11758bc
Dandrea Omar <omar_dandrea97@hotmail.com>
2023-08-29 01:25:35 -0300

    funcionalidad en la navegacion al ingresar mal la url

--- src/app/Views/page-not-found/page-not-found.component.ts
+++ src/app/Views/page-not-found/page-not-found.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { LoginApiService } from 'src/app/service/Login/login-api.service';
 
 @Component({
   selector: 'app-page-not-found',
@@ -7,9 +9,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PageNotFoundComponent implements OnInit {
 
-  constructor() { }
+  banderaRutas: string = '';
+  rutaURL: any;
+  constructor( private loginService:LoginApiService,private router: Router) {
+
+    debugger
+
+    this.rutaURL = window.location.pathname.split('/');
+
+
+    if(this.loginService.estaLogeado() == true){
+      //bandera utilizado para mostrar el menu de redireccion de la app
+      this.banderaRutas = 'Logueado'
+    }else{
+      //solo aparece el link para ir al login porque no esta registrado
+      this.banderaRutas = 'Desconectado'
+
+    }
+
+
+  }
 
   ngOnInit(): void {
+  }
+
+  navegacion(ubicacion:string){
+    if(ubicacion=='dashboard'){
+      this.router.navigate(['menu',this.rutaURL[2],'dashboard']);
+    }else if(ubicacion =='reclamo'){
+
+    }else{
+      // historial
+    }
   }
 
 }
