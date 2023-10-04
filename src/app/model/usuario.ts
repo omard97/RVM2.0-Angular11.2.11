1d2aec33711fb56e6c79a9af729686618fafb245
Dandrea Omar <omar_dandrea97@hotmail.com>
2023-10-03 21:08:51 -0300

    modelos usuario para configurarlos

--- src/app/model/usuario.ts
+++ src/app/model/usuario.ts
@@ -3,3 +3,28 @@ export class nickUsuario {
     nick: string = '';
     correo: string = '';
 }
+
+
+export interface usuarioConfig{
+
+    idUsuario: number;
+    nombreUsuario : string;
+    apellido : string,
+    telefono : string,
+    dni : string,
+    email : string,
+    nick : string,
+    foto? : string,
+
+    idEstado : number,
+    nombreEstado : string,
+    idTipoEstado : number,
+    nombreTipoEstado : string,
+}
+
+export class estadosUsuarios{
+    idEstado : number = 0;
+    nombre : string = '';
+    idTipoEstado : number = 0;
+    nombreTipoEstado : string = '';
+}
