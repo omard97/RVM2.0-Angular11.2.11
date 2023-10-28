70d62132cbd440162517c0941f462b6ae3a43dd0
Dandrea Omar <omar_dandrea97@hotmail.com>
2023-10-27 22:56:59 -0300

    Modelo coordenadas para mostar en el mapa

---
+++ src/app/model/Mapa/coordenadas.ts
@@ -0,0 +1,23 @@
+//Clase utilizada para visualizar los marcadores en el mapa del componente mapa con la longitud y latitud
+//la demas información se usa para mostrar la tarjeta al hacer click en el marcador
+export class coordenadas {
+    iDDetalleReclamo : number = 0;
+    descripcion : string='';
+    altura : string='' ;
+    longitud: number = 0;
+    latitud: number = 0;
+    direccion :string='' ;
+    iD_Reclamo :  number =0;
+    idReclamo :number = 0 ;
+    id_sesion : number = 0;
+    fecha : string='';
+    hora :string='' ;
+    nombre :string='' ;
+    iDEstado : number = 0;
+    nombreTRec :string='' ;
+    iDTipoRec : number = 0 ;
+    nombreRecAmbiental :string='' ;
+    nick :string='' ;
+    foto : string='';
+    dominio : string='';
+}
